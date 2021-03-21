59fda02bf0515669f0024f165e75a4b08b965880
Barath Raja <barath864148@gmail.com>
2021-03-21 23:13:01 +0000

    Initial commit.

---
+++ main.py
@@ -0,0 +1,7 @@
+#This python code will create a folder, resume and cover letter using a
+# base resume, cover letter with input of company, role name and optional job desc applying for
+#ex 1: if job desc is not provided 
+#       folder name: CompanyName_RoleName
+#       resume name: BarathRaja_RoleName resume
+#       coverletter: BarathRaja_RoleName CoverLetter
+#       Job Desc   : RoleName_JobDesc
